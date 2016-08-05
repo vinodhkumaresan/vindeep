3e638d37fded998bbd861ab62848d855a909b8be
vinodhkumaresan <vinodhkumaresan@users.noreply.github.com>
2016-08-05 16:24:34 +0530

    Add files via upload

--- route/mainroute.js
+++ route/mainroute.js
@@ -1,1 +1,14 @@
-first
+mainApp.config(['$routeProvider',function($routeProvider) {
+    $routeProvider
+    .when('/login', {
+        templateUrl: 'login.html',
+        controller: 'clair.controller.loginController'
+     })
+    .when('/loginSuccess', {
+	    templateUrl: 'loginValidation.html',
+	    //controller: 'clair.controller.loginController'
+     })
+     .when('/loginFailure', {
+    	templateUrl: 'loginFailure.html',
+     });
+}]);

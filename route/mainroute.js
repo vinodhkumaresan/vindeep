28fd6be3782a643444f79755136abcb91097d74a
vinodhkumaresan <vinodhkumaresan@users.noreply.github.com>
2016-08-14 22:16:47 +0530

    Add files via upload

--- route/mainroute.js
+++ route/mainroute.js
@@ -1,14 +1,22 @@
-mainApp.config(['$routeProvider',function($routeProvider) {
+mainApp.config(['$routeProvider', '$locationProvider',function($routeProvider, $locationProvider) {
     $routeProvider
     .when('/login', {
         templateUrl: 'login.html',
         controller: 'clair.controller.loginController'
      })
     .when('/loginSuccess', {
-	    templateUrl: 'homemain.html',
-	    //controller: 'clair.controller.loginController'
+	    templateUrl: 'wateranalysismain.html',
+	    controller: 'clair.controller.wateranalysisController'
      })
      .when('/loginFailure', {
     	templateUrl: 'loginFailure.html',
+     })
+     .when('/waterAnalysisCW', {
+	    templateUrl: 'wateranalysisCW.html',
+	    controller: 'clair.controller.cwController'
+     })
+     .when('/chemicalAnalysis', {
+	    templateUrl: 'chemicalanalysis.html',
+	    controller: 'clair.controller.chemicalanalysisController'
      });
 }]);
